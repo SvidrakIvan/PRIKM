7ecb8f9f0bc7f461473326c7cb1c5c022110fa2e
SvidrakIvan <ivan.svidrak.mitpa.2023@lpnu.ua>
2024-06-06 21:36:56 +0000

    test commit

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM nginx:latest
+RUN rm -rf /usr/share/nginx/html/index.html
+COPY ./honey5-main/. /usr/share/nginx/html/
---
+++ honey5-main/js/main.js
@@ -0,0 +1,5 @@
+$(function () {
+	$('.menu-open').click(function () {
+		$('.navigation').toggleClass('show-menu')
+	})
+});
